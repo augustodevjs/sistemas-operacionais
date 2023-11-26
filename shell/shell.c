da419d4431660a338ea6b5d9c3236607bbcd9d86
augusto <augustosaaude@gmail.com>
2023-11-26 16:42:38 -0300

    feat: add jantas-dos-filosofos

--- shell/shell.c
+++ shell/shell.c
@@ -7,16 +7,19 @@
 #include <sys/types.h>
 
 /*
-    Aqui, MAX_ARGS define o número máximo de argumentos que o programa pode aceitar, e TRUE é uma constante representando o valor lógico "verdadeiro". A matriz argumentos armazena os argumentos do comando, e temporario é uma matriz temporária para armazenar a entrada do usuário.
+    As constantes MAX_ARGS e TRUE são definidas. MAX_ARGS é o número máximo de argumentos que o programa pode aceitar. TRUE é uma constante representando o valor lógico "verdadeiro".
 */
 #define MAX_ARGS 64
 #define TRUE 1
 
+/*
+    entrada_usuario é uma string que armazena a entrada do usuário. args é um array de ponteiros para strings, que armazena os argumentos do comando.
+*/
 char entrada_usuario[64];
 char *args[MAX_ARGS];
 
 /*
-    Esta função imprime #Inserir comando: na tela, servindo como um indicador para o usuário inserir um comando.
+    A função mostrar_prompt() imprime um prompt na tela para o usuário inserir um comando.
 */
 void mostrar_prompt()
 {
@@ -24,7 +27,7 @@ void mostrar_prompt()
 }
 
 /*
-    ler_comando() lê a entrada do usuário usando fgets(), remove a quebra de linha final e, em seguida, divide a entrada em argumentos usando strtok().
+    A função ler_entrada() lê a entrada do usuário usando fgets(), remove a quebra de linha final e divide a entrada em argumentos usando strtok(). Os argumentos são armazenados no array args.
 */
 void ler_entrada(char *args[])
 {
@@ -43,7 +46,11 @@ void ler_entrada(char *args[])
 }
 
 /*
-    Um loop infinito (while(TRUE)) mantém o programa em execução, aceitando comandos continuamente. Exibir_prompt() mostra o prompt, e ler_comando() obtém o comando do usuário. Fork() cria um novo processo. O processo pai espera pelo filho usando wait(NULL). No processo filho, execvp() executa o comando inserido pelo usuário. Se houver algum erro, perror() exibe uma mensagem de erro.
+    A função main() contém um loop infinito que mantém o programa em execução. Dentro do loop, o prompt é exibido, a entrada é lida e um novo processo é criado usando fork().
+
+    Se fork() retorna -1, um erro ocorreu e o programa termina com uma mensagem de erro. Se fork() retorna um valor diferente de zero, estamos no processo pai e esperamos o processo filho terminar usando wait(NULL).
+
+    Se fork() retorna zero, estamos no processo filho. O comando do usuário é executado usando execvp(). Se execvp() retorna -1, um erro ocorreu e o programa termina com uma mensagem de erro.
 */
 int main()
 {
